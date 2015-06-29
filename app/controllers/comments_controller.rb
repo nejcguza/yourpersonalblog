bbdc91e58efd2c477f62f1ddb63d57482bc0f8f0
David Spegel <davidspeg@gmail.com>
2015-06-29 23:12:49 +0200

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create!(params.require(:comment).permit!) 
+    @comment.save
+
+
+    redirect_to @post
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])	
+    @comment.destroy
+
+    redirect_to @comment.post
+  end
+end
